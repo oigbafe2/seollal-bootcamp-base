4b0a9d80962a74d80641512b29012b2c94968cf4
wsl <wsl@wslui-MacBookPro-3.local>
2025-01-27 14:41:40 +0900

    my second update

--- src/app/main.py
+++ src/app/main.py
@@ -1,2 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.settings import Settings
+
+app = FastAPI()
+settings = Settings()
+
+
 def main():
-    print("Hello world")
+    uvicorn.run(
+        "app.main:app",
+        host=settings.host_address,
+        port=settings.port,
+        log_level="debug",
+    )
+    # print("Hello world")
---
+++ src/app/settings.py
@@ -0,0 +1,6 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    host_address: str = "0.0.0.0"
+    port: int = 8000
